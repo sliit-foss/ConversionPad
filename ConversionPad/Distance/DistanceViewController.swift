2ddd634f29e9aa8f2440abaab547d39d385d6937
rama41222 <dinushankanrg@gmail.com>
2018-08-19 03:10:24 +0530

    connecting ui view to controller

--- ConversionPad/Distance/DistanceViewController.swift
+++ ConversionPad/Distance/DistanceViewController.swift
@@ -9,9 +9,30 @@
 import UIKit
 
 class DistanceViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblOutputValue: UILabel!
+    @IBOutlet weak var lblInputUnit: UILabel!
+    @IBOutlet weak var lblInputValue: UITextField!
+    @IBOutlet weak var distancePicker: UIPickerView!
+    
+    let distances = Distance.toList()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        distancePicker.dataSource = self
+        distancePicker.delegate = self
         self.hideKeyboardWhenTappedAround()
     }
+}
+
+extension DistanceViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return distances.count
+    }
+    
+    
 }
