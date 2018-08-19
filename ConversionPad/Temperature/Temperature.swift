9d5ca1998d3628cb8bc44a553aef5f313aef6b41
rama41222 <dinushankanrg@gmail.com>
2018-08-19 10:48:57 +0530

    tempretature controller architecture refactor

--- ConversionPad/Temperature/Temperature.swift
+++ ConversionPad/Temperature/Temperature.swift
@@ -7,3 +7,96 @@
 //
 
 import Foundation
+
+enum Temperature: Int {
+    case celcius = 0, fahrenheit, kelvin
+    
+    func convertTo(temperature to: Temperature, value val: Double) -> Double {
+        
+        switch self {
+        case .celcius:
+            if to == .fahrenheit {
+                return celciusToFarenhiet(val)
+            } else if to == .kelvin {
+                return celciusToKelvin(val)
+            }  else {
+                return 0.0
+            }
+
+        case .fahrenheit:
+            if to == .celcius {
+                return farenhietToCelcius(val)
+            } else if to == .kelvin {
+                return farenhietToKelvin(val)
+            }  else {
+                return 0.0
+            }
+            
+        case .kelvin:
+            if to == .celcius {
+                return kelvinToCelcius(val)
+            } else if to == .fahrenheit {
+                return kelvinToFarenhiet(val)
+            }  else {
+                return 0.0
+            }
+        }
+    }
+    
+    static func fromString(_ string: String) -> Temperature? {
+        if string == "celcius" {
+            return .celcius
+        } else if string == "fahrenheit" {
+            return .fahrenheit
+        } else if string == "kelvin" {
+            return .kelvin
+        } else {
+            return nil
+        }
+    }
+    
+    func stringValue() -> String {
+        switch self {
+        case .celcius:
+            return "celcius"
+        case .fahrenheit:
+            return "fahrenheit"
+        case .kelvin:
+            return "kelvin"
+        }
+    }
+    
+    static func toList() -> [String] {
+        var i = 0
+        var list = [String]()
+        while let temperature = Temperature(rawValue: i) {
+            list.append(temperature.stringValue())
+            i = i + 1
+        }
+        return list
+    }
+    
+    func celciusToFarenhiet(_ celcius: Double) -> Double {
+        return 0.0
+    }
+    
+    func celciusToKelvin(_ celcius: Double) -> Double {
+        return 0.0
+    }
+    
+    func farenhietToCelcius(_ farenhiet: Double) -> Double {
+        return 0.0
+    }
+    
+    func farenhietToKelvin(_ farenhiet: Double) -> Double {
+        return 0.0
+    }
+    
+    func kelvinToCelcius(_ kelvin: Double) -> Double {
+        return 0.0
+    }
+    
+    func kelvinToFarenhiet(_ kelvin: Double) -> Double {
+        return 0.0
+    }
+}
