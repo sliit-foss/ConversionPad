8865f1d2117e2f788182f3480b629cfb379d33e4
rama41222 <dinushankanrg@gmail.com>
2018-08-19 09:09:26 +0530

    speed model calculations

--- ConversionPad/Speed/Speed.swift
+++ ConversionPad/Speed/Speed.swift
@@ -7,3 +7,123 @@
 //
 
 import Foundation
+
+enum Speed: Int {
+    case mps = 0, ftps, kmph, mph, knot
+    
+    func convertTo(speed to: Speed, value val: Double) -> Double {
+        var constant = 1.0
+        
+        switch self {
+            
+        case .mps:
+            if to == .ftps {
+                constant = 3.28084
+            } else if to == .kmph {
+                constant = 3.6
+            } else if to == .mph {
+                constant = 2.23694
+            } else if to == .knot {
+                constant = 1.94384
+            } else {
+                constant = 1
+            }
+            break
+        case .ftps:
+            if to == .mps {
+                constant = 0.3048
+            } else if to == .kmph {
+                constant = 1.09728
+            } else if to == .mph {
+                constant = 0.681818
+            } else if to == .knot {
+                constant = 0.592484
+            } else {
+                constant = 1
+            }
+            break
+        case .kmph:
+            if to == .mps {
+                constant = 0.277778
+            } else if to == .ftps {
+                constant = 0.911344
+            } else if to == .mph {
+                constant = 0.621371
+            } else if to == .knot {
+                constant = 0.539957
+            } else {
+                constant = 1
+            }
+            break
+        case .mph:
+            if to == .mps {
+                constant = 0.44704
+            } else if to == .ftps {
+                constant = 1.46667
+            } else if to == .kmph {
+                constant = 1.60934
+            } else if to == .knot {
+                constant = 0.868976
+            } else {
+                constant = 1
+            }
+            break
+        case .knot:
+            if to == .mps {
+                constant = 0.514444
+            } else if to == .ftps {
+                constant = 1.68781
+            } else if to == .kmph {
+                constant = 1.852
+            } else if to == .mph {
+                constant = 1.15078
+            } else {
+                constant = 1
+            }
+            break
+        }
+        return val * constant
+    }
+    
+    static func fromString(_ string: String) -> Speed? {
+        if string == "knot" {
+            return .knot
+        } else if string == "meters per second" {
+            return .mps
+        } else if string == "feet per second" {
+            return .ftps
+        } else if string == "miles per hour" {
+            return .mph
+        } else if string == "kilometers per hour" {
+            return .kmph
+        } else {
+            return nil
+        }
+    }
+    
+    func stringValue() -> String {
+        switch self {
+        case .knot:
+            return "knot"
+        case .mps:
+            return "meters per second"
+        case .ftps:
+            return "feet per second"
+        case .mph:
+            return "miles per hour"
+        case .kmph:
+            return "kilometers per hour"
+    
+        }
+    }
+    
+    static func toList() -> [String] {
+        var i = 0
+        var list = [String]()
+        while let speed = Speed(rawValue: i) {
+            list.append(speed.stringValue())
+            i = i + 1
+        }
+        return list
+    }
+}
