92fcd850408b203f8f814136a25c2a317811aab5
vishal-i4gs <vishal.i4gs@gmail.com>
2018-10-27 13:44:54 +0530

    Added Speed related conversion constants
    This is one of the possible ways of cleaning hardcoded constants in the code.

--- ConversionPad/Configuration/Constants.swift
+++ ConversionPad/Configuration/Constants.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+enum SpeedConst {
+ enum mps {
+  static let tomps = 1
+  static let toftps = 3.28084
+  static let tokmph = 3.6
+  static let tomph = 2.23694
+  static let toknot = 1.94384
+ }
+ enum ftps {
+  static let tomps = 0.3048
+  static let toftps = 1
+  static let tokmph = 1.09728
+  static let tomph = 0.681818
+  static let toknot = 0.592484
+ }
+ enum kmph {
+  static let tomps = 0.277778
+  static let toftps = 0.911344
+  static let tokmph = 1
+  static let tomph = 0.621371
+  static let toknot = 0.539957
+ }
+ enum mph {
+  static let tomps = 0.44704
+  static let toftps = 1.46667
+  static let tokmph = 1.60934
+  static let tomph = 1
+  static let toknot = 0.868976
+ }
+ enum knot {
+  static let tomps = 0.514444
+  static let toftps = 1.68781
+  static let tokmph = 1.852
+  static let tomph = 1.15078
+  static let toknot = 1
+ }
+}
