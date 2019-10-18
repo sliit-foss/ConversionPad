b6a34fa8980531d33d5da951bb59439e8c5edef9
Lareen Melo <lareenmelo@gmail.com>
2019-10-17 23:31:43 -0400

    Add Constants group

---
+++ ConversionPad/Configuration/Constants/FrequencyConstant.swift
@@ -0,0 +1,21 @@
+//
+//  FrequencyConstant.swift
+//  ConversionPad
+//
+//  Created by Lareen Melo on 10/17/19.
+//  Copyright © 2019 Dinushanka Nayomal. All rights reserved.
+//
+
+import Foundation
+
+enum FrequencyConst {
+    enum hz {
+        static let hz = 1.0
+        static let rpm = 60.0
+    }
+    
+    enum rpm {
+        static let hz = 0.01666666666
+        static let rpm = 1.0
+    }
+}
--- ConversionPad/Configuration/Constants/WeightConstant.swift
+++ ConversionPad/Configuration/Constants/WeightConstant.swift
@@ -1,64 +1,14 @@
 //
-//  Constants.swift
+//  WeightConstant.swift
 //  ConversionPad
 //
-//  Created by Dinushanka Nayomal on 8/19/18.
-//  Copyright © 2018 Dinushanka Nayomal. All rights reserved.
+//  Created by Lareen Melo on 10/17/19.
+//  Copyright © 2019 Dinushanka Nayomal. All rights reserved.
 //
 
 import Foundation
-enum SpeedConst {
- enum mps {
-  static let tomps = 1
-  static let toftps = 3.28084
-  static let tokmph = 3.6
-  static let tomph = 2.23694
-  static let toknot = 1.94384
-  static let toMach = 0.0030184123
- }
- enum ftps {
-  static let tomps = 0.3048
-  static let toftps = 1
-  static let tokmph = 1.09728
-  static let tomph = 0.681818
-  static let toknot = 0.592484
-  static let toMach = 0.00092001207
- }
- enum kmph {
-  static let tomps = 0.277778
-  static let toftps = 0.911344
-  static let tokmph = 1
-  static let tomph = 0.621371
-  static let toknot = 0.539957
-  static let toMach = 0.00083844787
- }
- enum mph {
-  static let tomps = 0.44704
-  static let toftps = 1.46667
-  static let tokmph = 1.60934
-  static let tomph = 1
-  static let toknot = 0.868976
-  static let toMach = 0.0013493510
- }
- enum knot {
-  static let tomps = 0.514444
-  static let toftps = 1.68781
-  static let tokmph = 1.852
-  static let tomph = 1.15078
-  static let toknot = 1
-  static let toMach = 0.0015528054
- }
- enum mach {
-  static let tomps = 343.00
-  static let toftps = 1125.33
-  static let tokmph = 1234.8021
-  static let tomph = 767.2704
-  static let toknot = 666.7397
-  static let toMach = 1
-  }
-}
 
-enum WeightConst {
+enum WeightConstant {
     enum gram {
         static let togram = 1
         static let tokilogram = 0.001
@@ -68,6 +18,7 @@ enum WeightConst {
         static let tomilligram = 1000.0
         static let tomicrogram = 1e+6
     }
+    
     enum kilogram {
         static let togram = 1000.0
         static let tokilogram = 1
@@ -77,6 +28,7 @@ enum WeightConst {
         static let tomilligram = 1e+6
         static let tomicrogram = 1e+9
     }
+    
     enum pound {
         static let togram = 453.592
         static let tokilogram = 0.453592
@@ -86,6 +38,7 @@ enum WeightConst {
         static let tomilligram = 453592.0
         static let tomicrogram = 453591999.86863
     }
+    
     enum ounce {
         static let togram = 28.3495
         static let tokilogram = 0.0283495
@@ -95,6 +48,7 @@ enum WeightConst {
         static let tomilligram = 28349.5
         static let tomicrogram = 2.835e+7
     }
+    
     enum ton {
         static let togram = 1e+6
         static let tokilogram = 1000.0
@@ -104,6 +58,7 @@ enum WeightConst {
         static let tomilligram = 1e+9
         static let tomicrogram = 1e+12
     }
+    
     enum milligram {
         static let togram = 0.001
         static let tokilogram = 1e-6
@@ -113,6 +68,7 @@ enum WeightConst {
         static let tomilligram = 1
         static let tomicrogram = 1000.0
     }
+    
     enum microgram {
         static let togram = 1e-6
         static let tokilogram = 1e-9
@@ -122,15 +78,4 @@ enum WeightConst {
         static let tomilligram = 0.001
         static let tomicrogram = 1
     }
-}
-
-enum FrequencyConst {
- enum hz {
-  static let hz = 1.0
-  static let rpm = 60.0
- }
- enum rpm {
-  static let hz = 0.01666666666
-  static let rpm = 1.0
- }
 }
